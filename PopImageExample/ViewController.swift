2158d19539efc9ee1a2544559ddfafb4ee045ada
CJ Willis <cjwillis809@gmail.com>
2020-06-06 22:29:13 -0500

    Initial commit

--- PopImageExample/ViewController.swift
+++ PopImageExample/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var chosenPhotoView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func chooseButtonPressed(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
+    }
+    
+    // MARK: UIImagePickerController Delegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            chosenPhotoView.image = image
+        } else {
+            print("No image found.")
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // TODO: Read up on UIContextMenuInteraction for the long press behavior.
+    
 }
 
